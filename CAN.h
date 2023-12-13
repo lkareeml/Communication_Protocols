51f6df4a7e89a4746614d1bacc7700f70299defa
kareem atef <kareem.atef@yahoo.com>
2023-12-13 14:58:07 +0200

    Update CAN.h

--- CAN.h
+++ CAN.h
@@ -1,3 +1,3 @@
 //Here, I will implement the CAN frame functionality.
 // START - Arbitration - Control(DLC) - Data - CRC() - ACK(bits) - ENDOFFRAME - IFS()
-
+//Wired logical and : 0 is domenent and 1 is not 
